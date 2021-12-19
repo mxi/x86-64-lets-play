80afaf32d50fe36d2d67dd59b93a840c69aed866
Maxim Kasyanenko <maxim.kasyanenko@protonmail.ch>
2021-12-18 21:45:44 -0800

    Fix unsigned division bug

--- ch14/stats_only.asm
+++ ch14/stats_only.asm
@@ -1,19 +1,4 @@
-%include "nisqrt_alone.asm"
-
-%macro call_stats 10
-	push     %10
-	push     %9
-	push     %8
-	push     %7
-	mov r9,  %6
-	mov r8,  %5
-	mov rcx, %4
-	mov rdx, %3
-	mov rsi, %2
-	mov rdi, %1
-	call stats
-	add rsp, 32 ; clear stack
-%endmacro
+%include "nisqrt_only.asm"
 
 %macro require_non_null 2
 	; %1 = ptr
@@ -23,33 +8,8 @@
 %endmacro
 
 
-section .data
-	Arr dq 1173, 1516, 1523, 2001, 2122,
-	    dq 2169, 2244, 2512, 2614, 2638,
-	    dq 2959, 3130, 3333, 3834, 3864,
-	    dq 3928, 4421, 4533, 4843, 4928,
-	    dq 5018, 5114, 5439, 5456, 5513,
-	    dq 5565, 5580, 5634, 5912, 5995,
-	    dq 6081, 6900, 7325, 7337, 7658,
-	    dq 7789, 7825, 7842, 7851, 8034,
-	    dq 8091, 8107, 8269, 8377, 8435,
-	    dq 8750, 9035, 9439, 9468, 9940,
-	
-	Len dq 50
-
-section .bss
-	Min  resq 1
-	Med1 resq 1
-	Med2 resq 1
-	Max  resq 1
-	Sum  resq 1
-	Avg  resq 1
-	Var  resq 1
-	Stdv resq 1
-
 section .text
 global stats
-global _start
 
 stats:
 	; Compute statistics of a sorted integer array.
@@ -154,7 +114,8 @@ stats_var_loop:
 
 	; compute stdev
 	push rdi
-	call_nisqrt rax
+	mov rdi, rax
+	call nisqrt
 	mov rbx, QWORD [rbp+40]
 	mov QWORD [rbx], rax
 	pop rdi
@@ -167,11 +128,3 @@ stats_end:
 	mov rsp, rbp
 	pop rbp
 ret
-
-_start:
-	call_stats Arr, QWORD [Len], Min, Med1, Med2, Max, Sum, Avg, Var, Stdv
-
-	; exit
-	mov rax, 60
-	mov rdi, 0
-	syscall
